189c877f060dfdee54e12ec7afa785cf5827e2ce
Chika Ani <casmonas@gmail.com>
2022-07-25 02:36:04 +0100

    [push]

--- public/productsandmaterials.php
+++ public/productsandmaterials.php
@@ -376,19 +376,86 @@
        
      <!-- Form starts here -->
               <form role="form"   method="post">
-          <?php
-
+              <h3>Quick Contact</h3>
+      <h4>Contact us today, and get reply with in 24 hours!</h4>
  
-                                        
-                                        ?>
+      <fieldset>
+        <input placeholder="Your name" name="name" type="text" tabindex="1" autofocus>
+      </fieldset>
+      <fieldset>
+        <input placeholder="Your Email Address" name="email" type="email" tabindex="2">
+      </fieldset>
+      <fieldset>
+        <input placeholder="Your Phone Number" name="tel" type="tel" tabindex="3" >
+      </fieldset>
+      <fieldset>
+        <input placeholder="Type your subject line" type="text" name="subject" tabindex="4">
+      </fieldset>
+      <fieldset>
+        <textarea name="message" placeholder="Type your Message Details Here..." tabindex="5"></textarea>
+      </fieldset>
+      <div>
+         <p class="success"> <?php echo $success;  ?></p>
+         <p class="failed"> <?php echo $failed;  ?></p>
+      </div>
+      <fieldset>
+        <button type="submit" name="submit" id="contact-submit" data-submit="...Sending">Submit Now</button>
+      </fieldset>
+
+
+
+
             <div class="modal-footer justify-content-between">
               <!-- <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button> -->
               <button type="submit" class="btn btn-primary" name="btnSave">Send</button>
             </div>
           </div>
               </form>
-      
-   
+
+
+              <?php  
+ 
+if(isset($_POST['submit'])) {
+ $mailto = "hmawebdesign@hotmail.com";  //My email address
+ //getting customer data
+ $name = $_POST['name']; //getting customer name
+ $fromEmail = $_POST['email']; //getting customer email
+ $phone = $_POST['tel']; //getting customer Phome number
+ $subject = $_POST['subject']; //getting subject line from client
+ $subject2 = "Confirmation: Message was submitted successfully | HMA WebDesign"; // For customer confirmation
+ 
+ //Email body I will receive
+ $message = "Cleint Name: " . $name . "\n"
+ . "Phone Number: " . $phone . "\n\n"
+ . "Client Message: " . "\n" . $_POST['message'];
+ 
+ //Message for client confirmation
+ $message2 = "Dear" . $name . "\n"
+ . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+ . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+ . "Regards," . "\n" . "- HMA WebDesign";
+ 
+ //Email headers
+ $headers = "From: " . $fromEmail; // Client email, I will receive
+ $headers2 = "From: " . $mailto; // This will receive client
+ 
+ //PHP mailer function
+ 
+  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+ 
+  //Checking if Mails sent successfully
+ 
+  if ($result1 && $result2) {
+    $success = "Your Message was sent Successfully!";
+  } else {
+    $failed = "Sorry! Message was not sent, Try again Later.";
+  }
+ 
+}
+ 
+?>
+
         
       </div><!-- /.container-fluid -->
     </section>
@@ -396,7 +463,7 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Footer<a href="http://adminlte.io"></a>.</strong>
+    <strong>Footer<a href="chikatech"></a>.</strong>
     
     <div class="float-right d-none d-sm-inline-block">
       
